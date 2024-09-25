81463cedd8d5ee9afb18692b6294e00fa48e6c31
Soni, Shubham <shubhamsoni9@deloitte.com>
2024-09-25 11:55:18 +0530

    Filer Implementation and Using filters to alter redux store data

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -1,19 +1,70 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import ProductCard from "./ProductCard";
 import "../utils/styles/ProductList.css";
+import { filterProducts } from "../utils/reduxStore/reduxSlices/productsSlice";
+import Slider from "rc-slider";
 
 const ProductsList = () => {
 
+    const dispatch = useDispatch();
+
+    const handleFilter = () => {
+        const collectionValue = document.getElementById('collection-filter').value;
+        const colorValue = document.getElementById('color-filter').value;
+        const categoryValue = document.getElementById('category-filter').value;
+
+        dispatch(filterProducts({collectionValue,colorValue,categoryValue}));
+    }
+
     const products = useSelector(state => state.products.filteredProducts);
-    console.log(products);
 
     return(
-        <div className="product-list">
-            { products.map(product => {
-                return <ProductCard product={product} key={product.id}/>
-            })}
-        </div>
+        <div className="product-list-container">
+            <div className="filter-container">
+                <div className="filter-container-heading">
+                    Filter By
+                </div>
+                <div className="filter-container-collection filter">
+                    <select onChange={handleFilter} id="collection-filter">
+                        <option value="">Collection</option>
+                        <option value="coombes">Coombes</option>
+                        <option value="keeveset">Keeve Set</option>
+                        <option value="kappu">Kappu</option>
+                        <option value="momo">Momo</option>
+                    </select>
+                </div>
+                <div className="filter-container-color filter">
+                    <select id="color-filter" onChange={handleFilter}>
+                        <option value="">Color</option>
+                        <option value="black">Black</option>
+                        <option value="white">White</option>
+                        <option value="yellow">Yellow</option>
+                        <option value="darkgrey">Dark Grey</option>
+                    </select>
+                </div>
+                <div className="filter-container-category filter">
+                    <select id="category-filter" onChange={handleFilter}>
+                        <option value="">Category</option>
+                        <option value="table&chair">Table & Chair</option>
+                        <option value="armchair">Armchair</option>
+                        <option value="selves">Selves</option>
+                        <option value="sidetable">Side Table</option>
+                    </select>
+                </div>
+                {/* <div className="filter filter-container-price">
+                    <h5>Price Range</h5>
+                    <span>$0</span>
+                    <Slider range min={0} max={10000} style={{flex:1, margin: '0 10px'}}/>
+                    <span>$10,000</span>
+                </div> */}
+            </div>
+            <div className="product-list">
+                { products.map(product => {
+                    return <ProductCard product={product} key={product.id}/>
+                })}
+            </div>
+        </div>    
     )
 }
 
--- src/utils/reduxStore/reduxSlices/productsSlice.js
+++ src/utils/reduxStore/reduxSlices/productsSlice.js
@@ -10,12 +10,20 @@ const productsSlice = createSlice({
         initializeProducts: (state, action) => {
             state.products = action.payload;
             state.filteredProducts = action.payload;
+        },
+        filterProducts: (state, action) => {
+            console.log(action.payload);
+            state.filteredProducts = state.products.filter((product) => {
+                return (( action.payload.collectionValue === '' ? product : product.product_collection.toLowerCase().replaceAll(' ', '') === action.payload.collectionValue)
+                    && (action.payload.colorValue === '' ? product : product.color_options.split(",").includes(action.payload.colorValue))
+                    && (action.payload.categoryValue === '' ? product : product.product_category.toLowerCase().replaceAll(' ','') === action.payload.categoryValue ));
+            });
         }
     }
 });
 
 // export Actions which can be used to update Products
-export const { initializeProducts } = productsSlice.actions;
+export const { initializeProducts, filterProducts } = productsSlice.actions;
 
 // export reduces which can be added to main store
 export default productsSlice.reducer;
